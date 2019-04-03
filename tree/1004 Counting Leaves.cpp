c9bc76ef0195bef7a50b7ce6f7cc2e9c7c5b0cac
prismleong <981591636@qq.com>
2019-04-03 23:51:45 +0800

    Update 1004 Counting Leaves.cpp

--- tree/1004 Counting Leaves.cpp
+++ tree/1004 Counting Leaves.cpp
@@ -3,41 +3,38 @@
 #include <map>
 #include <vector>
 using namespace std;
-
-void bfs(map<string,vector<string> > &t){
-	string root = "01";
-	int layer = 1;
-	queue<string> q;
-	q.push_back(root);
-	while(q.size()){
-		string r = q.front();
-		if(t[r].size()>0){
-		}
-		else{
-			cout<<
-		}
+//题目明确给出数字限制时尽量使用线性表 
+vector<vector<int> > tree(100);
+vector<int> noden(100);
+int maxd=0;
+//d为当前深度 
+void dfs(int root,int d){
+	//到叶子 
+	if(tree[root].size()==0){
+		noden[d]++;
+		maxd=max(maxd,d);
+		return; 
 	}
+	for(int i=0;i<tree[root].size();i++)
+		dfs(tree[root][i],d+1);
 }
 
 int main(){
-	int n,m;
-	while(cin>>n>>m){
-		if(n==0)	break;
-		map<string,vector<string> > tree;
-		for(int i=0;i<m;i++){
-			string id;
-			int k;
-			cin>>id>>k;
-			vector<string> v(k);
-			for(int j=0;j<k;j++){
-				cin>>v[j];
-				if(tree.find(v[j])==tree.end()){
-					vector<string> temp;
-					map[v[j]]=temp;
-				}
-			}
-			map[id]=v;
+	int n,m,id,k,child;
+	cin>>n>>m;
+	for(int i=0;i<m;i++){
+		cin>>id>>k;
+		//cout<<id<<" "<<k;
+		//看清循环变量！ 
+		for(int j=0;j<k;j++){
+			cin>>child;
+			tree[id].push_back(child);
 		}
+	}
+	dfs(1,0);
+	cout<<noden[0];
+	for(int i=1;i<=maxd;i++){
+		cout<<" "<<noden[i];
 	}
 	return 0;
 } 
